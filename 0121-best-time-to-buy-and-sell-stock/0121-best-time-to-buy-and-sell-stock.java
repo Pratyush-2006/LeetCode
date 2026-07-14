cc185e918d7bda44c50174698d4ed35bbb487deb
Prayush Gupta <pratyush.gupta.in@gmail.com>
2026-07-14 07:53:37 +0530

    Time: 1 ms (99.94%), Space: 94.6 MB (33.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,11 +3,12 @@ class Solution {
         int buy_price = prices[0];
         int profit=0;
 
-        for(int i=1; i<prices.length; i++){
-                if(prices[i]<buy_price){
-                    buy_price=prices[i];                }
+        for(int i : prices){
+                if(i<buy_price){
+                    buy_price=i;                
+                }
                 else{
-                    int current_profit=prices[i]-buy_price;
+                    int current_profit=i-buy_price;
                     profit = Math.max(profit, current_profit);
                 }
         }
